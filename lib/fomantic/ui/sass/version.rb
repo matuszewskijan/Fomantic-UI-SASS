e310df0f060a67555094b7bc4042396ab134e9d2
Jan Matuszewski <matuszewski.jan@hotmail.com>
2025-07-28 14:38:10 +0200

    New assets versions
    Generated through `rake convert`. It's compiled fomantic UI with site.variables defined by us.

--- lib/fomantic/ui/sass/version.rb
+++ lib/fomantic/ui/sass/version.rb
@@ -4,7 +4,7 @@ module Fomantic
   module Ui
     module Sass
       VERSION = '2.9.3.2'
-      SEMANTIC_UI_SHA = '697bba341f4ea6357e5fe22d8c7166aec107bf2a'
+      SEMANTIC_UI_SHA = '2df56a858673a131dacf5165218f0b4b231bc525'
     end
   end
 end
